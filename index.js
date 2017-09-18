03252f949841ba1859ab4e9bb9a97950f6dba5f5
Fernando Henrique Coelho Farias <nandofarias@me.com>
2017-09-18 16:24:04 -0300

    Logger entry

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require('./infrastructure/server');
+const logger = require('./infrastructure/logger');
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 
 require('./infrastructure/mongoose');
 require('./infrastructure/socketio');
 require('./controllers');
 
-server.listen(port, () => console.log(`Server listing on port: ${port}`));
+server.listen(port, () => logger.info(`Server listing on port: ${port}`));
